825c0a061cbefdd8e8392018154711f71c5b9399
collinalexbell <collinalexbell@gmail.com>
2024-06-07 15:42:48 -0400

    binary search

--- src/leetcode/search-insert-position.ts
+++ src/leetcode/search-insert-position.ts
@@ -1,19 +1,14 @@
-function searchInsertInd(nums: number[], target: number, start: number, end: number) {
-    if(start >= end-1) {
-        return start;
-    }
-
-    const ind = (end-start)/2 + start;
-    if(nums[ind] == target) {
-        return ind;
-    }
-    if(nums[ind] < target) {
-        return searchInsertInd(nums, target, start, ind);
-    } else {
-        return searchInsertInd(nums, target, ind, end);
-    }
-}
-
 function searchInsert(nums: number[], target: number): number {
-    return searchInsertInd(nums, target, 0, nums.length);
+    const index = Math.floor(nums.length / 2)
+    if(nums[index] == target) {
+        return index;
+    }
+    if(index == 0) {
+        return 0
+    }
+    if(nums[index] < target) {
+        return searchInsert(nums.slice(0,index), target);
+    } else {
+        return index + searchInsert(nums.slice(index), target);
+    }
 };
